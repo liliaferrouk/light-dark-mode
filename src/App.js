c1c7928940a32888d0a61b53b09d7bd0f350d844
ferrouklilia <125090363+liliaferrouk@users.noreply.github.com>
2023-04-23 20:08:19 +0100

    v1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+function App() {
+  const [darkMode, setDarkMode]= React.useState(true)
+
+  function toggleDarkMode(){
+    setDarkMode(prevstat => !prevstat)
+  }
+
+  return (
+    <div className="container">
+      <Navbar
+         darkMode={darkMode}
+         toggleDarkMode={toggleDarkMode}
+      />
+      <Main darkMode={darkMode}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import img from "../images/logo.png"
+
+export default function Navbar(props){
+    return(
+        <nav className={props.darkMode ? "dark" : ""}>
+
+            <img src={img} className="nav--logo_icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            
+            <div className="toggler">
+                <p className="toggler--light">Light</p>
+                <div className="toggler--slider" onClick={props.toggleDarkMode}>
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+
+        </nav>
+    )
+}
